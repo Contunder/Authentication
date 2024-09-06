3a6fea4ac980852d0742e8aabd416ba06f3f97a7
Valentin <75315884+Contunder@users.noreply.github.com>
2024-09-07 00:42:35 +0200

    Update dependency

--- src/main/java/com/helloworlds/authentication/AuthenticationApiApplication.java
+++ src/main/java/com/helloworlds/authentication/AuthenticationApiApplication.java
@@ -2,8 +2,14 @@ package com.helloworlds.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.helloworlds.authentication")
+@EnableJpaRepositories("com.helloworlds.authentication")
 public class AuthenticationApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/helloworlds/authentication/infrastructure/dao/AuthenticationDAO.java
+++ src/main/java/com/helloworlds/authentication/infrastructure/dao/AuthenticationDAO.java
@@ -2,9 +2,11 @@ package com.helloworlds.authentication.infrastructure.dao;
 
 import com.helloworlds.authentication.infrastructure.entity.Authentification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthenticationDAO extends JpaRepository<Authentification, Long> {
 
     Optional<Authentification> findByEmail(String email);
